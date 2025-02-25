a680c2d370d05d0d9ff83f8f94df0e7ebc9d090f
_Nibe <den27042004@gmail.com>
2025-02-26 01:22:23 +0200

    redesigned the project structure, add some html plugs

--- eventHub/eventhub/eventhub/settings.py
+++ eventHub/eventhub/eventhub/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    'participants',
+    'users',
     'checkin',
     'django.contrib.admin',
     'django.contrib.auth',
--- eventHub/eventhub/users/models.py
+++ eventHub/eventhub/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Participant(models.Model):
+class User(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True, max_length=100)
 
 
-    def __str__(self):
+    def str(self):
         return self.full_name
