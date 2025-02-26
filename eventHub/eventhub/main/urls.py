ef1681d0a0a6d03764206d38bdcb1743c236e1b1
_Nibe <den27042004@gmail.com>
2025-02-26 10:28:22 +0200

    create urls for main app and users app

--- eventHub/eventhub/main/urls.py
+++ eventHub/eventhub/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
 ]
--- eventHub/eventhub/users/views.py
+++ eventHub/eventhub/users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def users(request):
+    users = render(request, 'users/users.html')
